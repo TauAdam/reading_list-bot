843481caa63cbfc50e2a386051f11d99faeef32c
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-02 17:20:31 +0500

    feat: updates

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,6 +1,12 @@
 package telegram
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
 
 type Client struct {
 	host     string
@@ -17,10 +23,32 @@ func New(host, token string) Client {
 
 }
 
-func (c *Client) Updates() {
+func (c *Client) Updates(offset, limit int) ([]Update, error) {
+	queryMap := url.Values{}
+	queryMap.Add("offset", strconv.Itoa(offset))
+	queryMap.Add("limit", strconv.Itoa(limit))
 
 }
 
 func (c *Client) SendMessage() {
 
+}
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %w", err)
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not send request: %w", err)
+	}
 }
