b81010c54410d1d012c2dc9ebe888e46831c874f
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-02 23:07:23 +0500

    feat: generate file name based on hash of content

--- storage/file-based/file-based.go
+++ storage/file-based/file-based.go
@@ -1,1 +1,39 @@
 package file_based
+
+import (
+	"github.com/tauadam/reading_list-bot/lib/utils"
+	"github.com/tauadam/reading_list-bot/storage"
+	"os"
+	"path/filepath"
+)
+
+type Storage struct {
+	basePath string
+}
+
+func New(basePath string) Storage {
+	return Storage{
+		basePath,
+	}
+}
+
+func (s Storage) Save(article *storage.Article) (err error) {
+	defer func() { err = utils.Wrap("can't save", err) }()
+
+	filePath := filepath.Join(s.basePath, article.UserName)
+
+	if err := os.MkdirAll(filePath, 0744); err != nil {
+		return err
+	}
+
+	fileName, err := generateFileName(article)
+	if err != nil {
+		return err
+	}
+
+	//	TODO write to file
+}
+
+func generateFileName(a *storage.Article) (string, error) {
+	return a.Hash()
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,13 +1,34 @@
 package storage
 
+import (
+	"crypto/sha1"
+	"fmt"
+	"github.com/tauadam/reading_list-bot/lib/utils"
+	"io"
+)
+
 type Storage interface {
 	Save(a *Article) error
 	PickRandom(userName string) (*Article, error)
-	Remove()
-	IsExist() bool
+	Remove(a *Article) error
+	IsExist(a *Article) (bool, error)
 }
 
 type Article struct {
 	URL      string
 	UserName string
+}
+
+func (a Article) Hash() (string, error) {
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, a.URL); err != nil {
+		return "", utils.Wrap("can't calc hash", err)
+	}
+
+	if _, err := io.WriteString(h, a.UserName); err != nil {
+		return "", utils.Wrap("can't calc hash", err)
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
